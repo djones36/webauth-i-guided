2f9de80518bbb2d93564a1c6090ff7034ab9e04a
Daniel Jones <daniel.jones2000@gmail.com>
2019-10-14 10:54:48 -0700

    branch

--- index.js
+++ index.js
@@ -50,10 +50,12 @@ server.post('/api/register', (req, res) => {
 server.post('/api/login', (req, res) => {
   let { username, password } = req.body;
 
+
   Users.findBy({ username })
     .first()
     .then(user => {
-      if (user) {
+
+      if (user && bcrypt.compareSync(password, user.password)) {
         res.status(200).json({ message: `Welcome ${user.username}!` });
       } else {
         res.status(401).json({ message: 'Invalid Credentials' });
@@ -64,13 +66,26 @@ server.post('/api/login', (req, res) => {
     });
 });
 
-server.get('/api/users', (req, res) => {
+server.get('/api/users', protected, (req, res) => {
   Users.find()
     .then(users => {
       res.json(users);
     })
     .catch(err => res.send(err));
 });
+
+//implement the proctected middlewar that will check for user name and password
+//in the headers and if valid provide access to the endpoint
+function protected() {
+
+  const { password, user } = req.headers.authorization;
+  if (user && bcrypt.compareSync(password, user.password)) {
+    next()
+  } else {
+    res.status(401).json({ message: 'wrong password' })
+  }
+
+}
 
 const port = process.env.PORT || 5000;
 server.listen(port, () => console.log(`\n** Running on port ${port} **\n`));
